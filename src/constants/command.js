a9beefe8cd13d9b480cd6d7e2be9f48b3d2dc8ac
Kevin Truong <50157737+SignatureKT@users.noreply.github.com>
2020-10-12 11:43:02 -0700

    add comments to command.js

--- src/constants/command.js
+++ src/constants/command.js
@@ -1,11 +1,11 @@
-export const COMMAND_DICE = '!dice';
-export const COMMAND_LOOKUP = '!look';
-export const COMMAND_HASH = '!hash';
-export const COMMAND_CURRENCY = '!currency'
-export const COMMAND_GAMBLE = '!gamble';
-export const COMMAND_TOP = '!top';
-export const COMMAND_BET = '!bet';
-export const COMMAND_DUEL = '!duel';
-export const COMMAND_ACCEPT = '!accept';
+export const COMMAND_DICE = '!dice'; // roll a six sided dice
+export const COMMAND_LOOKUP = '!look'; // (NOT AVAILABLE) look up another user
+export const COMMAND_HASH = '!hash'; // (NOT AVAILABLE/ FOR DEVOLOPMENT PURPOSES)
+export const COMMAND_CURRENCY = '!currency' // look up the user's currency
+export const COMMAND_GAMBLE = '!gamble'; // enter the ammount 
+export const COMMAND_TOP = '!top'; // leaderboard of the top 5 currency
+export const COMMAND_BET = '!bet'; // bet on win or lose
+export const COMMAND_DUEL = '!duel'; // challenge another user to a duel
+export const COMMAND_ACCEPT = '!accept'; // accepts a challenged duel
 
-export const EXTENSION_SHARE = 'share'
+export const EXTENSION_SHARE = 'share' // used in conjunction with COMMAND_CURRENCY to share to a user
